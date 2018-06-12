3007d901628ce3ae741155f9fec401ec89882bc6
Dawnfable <DuskDawnfable@gmail.com>
2018-06-12 12:41:15 -0600

    made full crud for statements

---
+++ app/controllers/statements_controller.rb
@@ -0,0 +1,66 @@
+class StatementsController < ApplicationController
+  before_action :set_statement, only: [:show, :edit, :update, :destroy, :toggle_status]
+
+  def index
+    @statements = Statement.all
+
+  end
+
+  def new
+    @statement = Statement.new
+  end
+
+  def create
+    @statement = Statement.new(statement_params)
+    if @statement.save
+      redirect_to statement_path(@statement)
+    else
+      render :new
+    end
+  end
+
+  def show
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+    if @statement.update(statement_params)
+      @statement.save
+      redirect_to @statement, notice: 'Your statement was edited successfully'
+    else
+      redirect_to :new, notice: 'Error editing your statement'
+    end
+  end
+
+  def destroy
+    if @statement.destroy
+      redirect_to statements_path, notice: 'Your statement was deleted'
+    else
+      render :show, notice: 'Error deleting your statement'
+    end
+  end
+
+  def toggle_status
+    if @statement.pending?
+      @statement.completed!
+    elsif @statement.completed?
+      @statement.pending!
+    end
+    redirect_to statements_path, notice: "#{@statement.date.strftime('%B %Y')} has been set to #{@statement.status}"
+  end
+
+  private
+
+  def set_statement
+    @statement = Statement.find(params[:id])
+  end
+
+  def statement_params
+    params.require(:statement).permit(:date)
+  end
+
+end
---
+++ app/models/statement.rb
@@ -0,0 +1,4 @@
+class Statement < ApplicationRecord
+  enum status: {pending: 0, completed: 1}
+  validates_presence_of :date
+end
