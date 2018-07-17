741eff6233d6ec20c3f95a80d4cc0c80531e647e
Dawnfable <DuskDawnfable@gmail.com>
2018-07-17 10:48:42 -0600

    fixed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 User.create!(
   email: 'admin@test.com',
+  first_name: 'Admin',
+  last_name: 'Test',
   password: 'asdfasdf',
   password_confirmation: 'asdfasdf',
   roles:'admin')
@@ -8,6 +10,8 @@ puts 'Admin user created'
 
 User.create!(
   email: 'test@test.com',
+  first_name: 'Reg',
+  last_name: 'User',
   password: 'asdfasdf',
   password_confirmation: 'asdfasdf',
   roles:'user')
